24bf1083b4eeb5b02467a1fe81edaec3c8b0209b
xkrik <krikonis.chr@gmail.com>
2019-06-14 04:25:30 +0300

    Images container added

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import './App.css';
 import DogListContainer from './components/DogListContainer';
 import { Route } from 'react-router-dom'
 import DogBreedImages from './components/DogBreedImages';
+import DogBreedImagesContainer from './components/DogBreedImagesContainer';
 
 function App() {
   return (
     <div className="App">
       <main>
         <Route exact path="/" component={DogListContainer} />
-        <Route exact path="/dog-breeds/:breed" component={DogBreedImages} />
+        <Route exact path="/dog-breeds/:breed" component={DogBreedImagesContainer} />
       </main>
     </div>
   );
--- src/components/DogBreedImages.js
+++ src/components/DogBreedImages.js
@@ -1,14 +1,26 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 
-export default function DogBreedImages (props) {
-    return (
-        <div className="dog-breed-images">
-            <h1>Dog Breed Images</h1>
+export default class DogBreedImages extends Component {
+    renderDogBreedImages (images) {
+        return <li key={images}>
+                 <Link to={ `/dog-images/${images}` }>{images}</Link>
+               </li>
+    }
 
-            This page will show images of the { props.match.params.breed} breed.
-
-            <Link to="/">Go back to index</Link>
-        </div>
-    )
+    render () {
+        const {images} = this.props
+        return (
+            <div className="dog-breed-images">
+                <h1>Dogs Breed Images</h1>
+                
+                { !images && 'Loading...' } 
+      
+                { images && images
+                .map(url => <img src={ url } alt="Dog" />) }
+     
+      
+            </div>
+        )
+    }
 }
